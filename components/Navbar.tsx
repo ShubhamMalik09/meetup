c8fb5ce1262750bfd0b9147b44900ccc9e530299
Shubham <shubhammalik.sm05@gmail.com>
2024-07-09 01:32:55 +0530

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import MobileNav from './MobileNav'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+
+const Navbar = () => {
+  return (
+    <nav className='flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+      <Link href={"/"} className='flex items-center gap-1'>
+        <Image src={"/icons/meetUp.png"} width={70} height={70} alt='MeetUp logo' className='max-sm:size-10'/>
+        <p className='text-[26px] font-extrabold text-white max-sm:hidden'>MeetUp</p>
+
+        <div className='flex-between gap-5'>
+          <SignedIn>
+            <UserButton/>
+          </SignedIn>
+
+          <SignedOut>
+            <SignInButton/>
+          </SignedOut>
+          <MobileNav/>
+        </div>
+      </Link>
+    </nav>
+  )
+}
+
+export default Navbar
